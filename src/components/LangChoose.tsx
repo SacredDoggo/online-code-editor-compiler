740d00f3ae5cc03f1c17a54bc7a56185bcb17d22
Om Vipul Krishna <omvkrishna0852@gmail.com>
2023-09-19 23:09:14 +0530

    better implementation with react-dropdown-select

--- src/components/LangChoose.tsx
+++ src/components/LangChoose.tsx
@@ -1,32 +1,32 @@
-function LangChoose () {
-	return (
-		<select name="language" id="language" className="p-1 rounded-sm bg-white ">
-			<option value="C">C</option>
-			<option value="CPP14">C++14</option>
-			<option value="CPP17">C++17</option>
-			<option value="CLOJURE">Clojure</option>
-			<option value="CSHARP">C#</option>
-			<option value="GO">Go</option>
-			<option value="HASKELL">Haskell</option>
-			<option value="JAVA8">Java 8</option>
-			<option value="JAVA14">Java 14</option>
-			<option value="JAVASCRIPT_NODE">JavaScript(Nodejs)</option>
-			<option value="KOTLIN">Kotlin</option>
-			<option value="OBJECTIVEC">Objective C</option>
-			<option value="PASCAL">Pascal</option>
-			<option value="PERL">Perl</option>
-			<option value="PHP">PHP</option>
-			<option value="PYTHON">Python 2</option>
-			<option value="PYTHON3">Python 3</option>
-			<option value="PYTHON3_8">Python 3.8</option>
-			<option value="R">R</option>
-			<option value="RUBY">Ruby</option>
-			<option value="RUST">Rust</option>
-			<option value="SCALA">Scala</option>
-			<option value="SWIFT">Swift</option>
-			<option value="TYPESCRIPT">TypeScript</option>
-		</select>
-	);
-};
+import Select from "react-dropdown-select";
 
+interface InputProps {
+	value: { value: number; label: string; }[];
+	onChange: any;
+}
+
+const LangChoose: React.FC<InputProps> = ({ value, onChange }) => {
+	const options = [
+		{ value: 1, label: "C" },
+		{ value: 2, label: "C++ 14" },
+		{ value: 3, label: "C++ 17" },
+		{ value: 4, label: "Python 2" },
+		{ value: 5, label: "Python 3" },
+		{ value: 6, label: "Python 3.8" },
+		{ value: 7, label: "Java 8" },
+		{ value: 8, label: "Java 14" },
+		{ value: 9, label: "Javascript(NodeJS)" }
+	];
+
+  return (
+    <Select
+	style={{
+        width: "100%",
+      }}
+	  values={value}
+      options={options}
+      onChange={onChange}
+    />
+  );
+}
 export default LangChoose;
