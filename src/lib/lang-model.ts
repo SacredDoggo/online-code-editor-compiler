49de6b051a3c807077ede8b7fa9c403c010dc84f
Om Vipul Krishna <omvkrishna0852@gmail.com>
2023-09-19 23:06:06 +0530

    changed keys to suit more operations

--- src/lib/lang-model.ts
+++ src/lib/lang-model.ts
@@ -1,55 +1,55 @@
 interface langModel {
     'hackerearthapi': string;
-    'mode-ace': string;
+    'mode_ace': string;
     'boilerplate': string;
 }
 
-const lang:{[key: string] : langModel} = {
-    'c' : {
+const lang_model:{[key: string] : langModel} = {
+    'C' : {
         'hackerearthapi': 'C',
-        'mode-ace': 'c_cpp',
-        'boilerplate' : '#include<stdio.h>\n\nint main()\n{\n\n\tprrintf("Hello, World!);\n\treturn 0;\n}'
+        'mode_ace': 'c_cpp',
+        'boilerplate' : '#include<stdio.h>\n\nint main()\n{\n\n\tprrintf("Hello, World! from C);\n\treturn 0;\n}'
     },
-    'cpp14' : {
+    'C++ 14' : {
         'hackerearthapi': 'CPP14',
-        'mode-ace': 'c_cpp',
-        'boilerplate' : '#include<iostream>\n\nint main()\n{\n\n\tstd::cout << \"Hello, World!\";\n\treturn 0;\n}'
+        'mode_ace': 'c_cpp',
+        'boilerplate' : '#include<iostream>\n\nint main()\n{\n\n\tstd::cout << \"Hello, World! from C++ 14\";\n\treturn 0;\n}'
     },
-    'cpp17' : {
+    'C++ 17' : {
         'hackerearthapi': 'CPP17',
-        'mode-ace': 'c_cpp',
-        'boilerplate' : '#include<iostream>\n\nint main()\n{\n\n\tstd::cout << \"Hello, World!\";\n\treturn 0;\n}'
+        'mode_ace': 'c_cpp',
+        'boilerplate' : '#include<iostream>\n\nint main()\n{\n\n\tstd::cout << \"Hello, World! from C++ 17\";\n\treturn 0;\n}'
     },
-    'python2' : {
+    'Python 2' : {
         'hackerearthapi': 'PYTHON',
-        'mode-ace': 'python',
-        'boilerplate' : 'print "Hello, OVK!"'
+        'mode_ace': 'python',
+        'boilerplate' : 'print "Hello, World! from Python 2"'
     },
-    'python3' : {
+    'Python 3' : {
         'hackerearthapi': 'PYTHON3',
-        'mode-ace': 'python',
-        'boilerplate' : 'print("Hello, OVK!")'
+        'mode_ace': 'python',
+        'boilerplate' : 'print("Hello, World! from Python 3")'
     },
-    'python3_8' : {
+    'Python 3.8' : {
         'hackerearthapi': 'PYTHON3_8',
-        'mode-ace': 'python',
-        'boilerplate' : 'print("Hello, OVK!")'
+        'mode_ace': 'python',
+        'boilerplate' : 'print("Hello, World! from Python 3.8")'
     },
-    'java8' : {
+    'Java 8' : {
         'hackerearthapi': 'JAVA8',
-        'mode-ace': 'java',
-        'boilerplate': 'public class Main {\n\tpublic static void main(String args[]) {\n\t\tSystem.out.print("Hello, World!");\n\t}\n}'
+        'mode_ace': 'java',
+        'boilerplate': 'public class Main {\n\tpublic static void main(String args[]) {\n\t\tSystem.out.print("Hello, World! from Java 8");\n\t}\n}'
     },
-    'java14' : {
+    'Java 14' : {
         'hackerearthapi': 'JAVA14',
-        'mode-ace': 'java',
-        'boilerplate': 'public class Main {\n\tpublic static void main(String args[]) {\n\t\tSystem.out.print("Hello, World!");\n\t}\n}'
+        'mode_ace': 'java',
+        'boilerplate': 'public class Main {\n\tpublic static void main(String args[]) {\n\t\tSystem.out.print("Hello, World! from Java 14");\n\t}\n}'
     },
-    'javascript' : {
+    'Javascript(NodeJS)' : {
         'hackerearthapi': 'JAVASCRIPT_NODE',
-        'mode-ace': 'java',
-        'boilerplate': 'console.log("Hello, World!");'
+        'mode_ace': 'java',
+        'boilerplate': 'console.log("Hello, World! from Javascript(NodeJS)");'
     }
 }
 
-export default lang;
+export default lang_model;
