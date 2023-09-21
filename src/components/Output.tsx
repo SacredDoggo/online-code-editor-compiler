12cb0e82123fb38182a74c3793f678539eb0a0d6
Om Vipul Krishna <omvkrishna0852@gmail.com>
2023-09-21 13:58:43 +0530

    data display done

--- src/components/Output.tsx
+++ src/components/Output.tsx
@@ -1,19 +1,35 @@
+import { useEffect, useState } from 'react';
 
 interface InputProps {
-    Output : {
-        output : string;
-        status : string;
-        status_detail : string;
-        time_used : number;
-        memory_used : number;
-    }
-};
-
-const Output: React.FC<InputProps> = ({ Output }) => {
-    return (
-        'Output'
-    )
+  output: {
+    output: string;
+    status: string;
+    status_detail: string;
+    time_used: number;
+    memory_used: number;
+  };
 }
+
+const Output: React.FC<InputProps> = ({ output }) => {
+    const rows = 3;
+    
+  return (
+    <div>
+      <div className="flex w-full justify-between px-2">
+        <span>Status: '{output.status}'</span>
+        <span>Time: {output.time_used}</span>
+        <span>Memory: {output.memory_used}</span>
+      </div>
+      <div>
+        <textarea
+          rows={rows}
+          value={output.output}
+          className="w-full resize-none rounded-sm text-sm py-0.5"
+        ></textarea>
+      </div>
+    </div>
+  );
+};
 
 export default Output;
 
